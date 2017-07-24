41abf48cf5158097ebede12361d1b29669bc560f
Leandro I. Farias <leandro.i.farias@avanade.com>
2017-07-24 18:35:06 -0300

    mensagem commit

--- js/principal.js
+++ js/principal.js
@@ -27,12 +27,14 @@ for(var i = 0; i < pacientes.length ; i++){
       console.log("Peso inválido");
       pesoEhValido = false;
       tdImc.textContent = "Peso inválido!";
+      paciente.classList.add("paciente-invalido");
     }
 
     if(altura <= 0 || altura >= 3.00){
       console.log("Altura inválida!");
       alturaEhValida = false;
       tdImc.textContent = "altura inválida!";
+      paciente.classList.add("paciente-invalido");
     }
 
     if (alturaEhValida && pesoEhValido) {
@@ -40,3 +42,55 @@ for(var i = 0; i < pacientes.length ; i++){
       tdImc.textContent = imc.toFixed(2);
     }
 }
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(){
+
+    event.preventDefault(); //previne um comportamento padrao e deixa que decida qual medida tomar
+
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    imcTd.textContent = imc;
+
+    pacienteTr.appendChild(nomeTd);//coloca como filho na estrutura
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr); //coloca todos os tr como filho da tabela, na estrutura.
+
+
+});
+
+
+
+//funcao anonima
+// titulo.addEventListener("click", function(){
+//     console.log("Olha só posso chamar um funcao anonima.");
+// });
+
+
+//funcao normal
+// function mostraMensagem(){
+//   console.log("Olá fui clicado!");
+// }
