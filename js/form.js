0816f2addb11ed961bfed616076cb8c9c0bbbbd7
Leandro I. Farias <leandro.i.farias@avanade.com>
2017-07-25 14:05:20 -0300

    botao cadastrar com validacoes funcionando

--- js/form.js
+++ js/form.js
@@ -1,7 +1,8 @@
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener("click", function(){//addEventListener escuta evento do browser
+botaoAdicionar.addEventListener("click", function(event){//addEventListener escuta evento do browser
 
     event.preventDefault(); //previne um comportamento padrao e deixa que decida qual medida tomar
+
 
     var form = document.querySelector("#form-adiciona");
 
@@ -11,6 +12,14 @@ botaoAdicionar.addEventListener("click", function(){//addEventListener escuta ev
     //CRIA A 'TR' E 'TD' DO PACIENTE
     var pacienteTr = montaTr(paciente);
 
+    var erros = validaPaciente(paciente);
+
+    if(erros.length > 0){
+        exibeMensagensDeErro(erros);
+
+        return;
+    }
+
     //ADICIONANDO  O PACIENTE NA TABELA
     var tabela = document.querySelector("#tabela-pacientes");
 
@@ -18,8 +27,10 @@ botaoAdicionar.addEventListener("click", function(){//addEventListener escuta ev
 
     form.reset(); //reset - limpa os campos
 
-});
+    var mensagensErro = document.querySelector("#mensagens-erro");
+    mensagensErro.innerHTML = "";
 
+});
 
 function obtemPacienteDoFormulario(form){
 
@@ -51,10 +62,62 @@ function montaTr(paciente){
 
 function montaTd(dado, classe){
   var td = document.createElement("td");
-  td.textContent = dado;
   td.classList.add(classe);
+  td.textContent = dado;
   return td;
 }
+
+function validaPaciente(paciente) {
+
+    var erros = [];
+
+    if (paciente.nome.length == 0) {
+        erros.push("O nome não pode ser em branco");
+    }
+
+    if (paciente.gordura.length == 0) {
+        erros.push("A gordura não pode ser em branco");
+    }
+
+    if (paciente.peso.length == 0) {
+        erros.push("O peso não pode ser em branco");
+    }
+
+    if (paciente.altura.length == 0) {
+        erros.push("A altura não pode ser em branco");
+    }
+
+    if (!validaPeso(paciente.peso)) {
+        erros.push("Peso é inválido");
+    }
+
+    if (!validaAltura(paciente.altura)) {
+        erros.push("Altura é inválida");
+    }
+
+    return erros;
+}
+
+
+function exibeMensagensDeErro(erros){
+  var ul = document.querySelector("#mensagens-erro");
+  ul.innerHTML = "";
+
+
+  erros.forEach(function(erro){
+    var li= document.createElement("li");
+    li.textContent = erro;
+    ul.appendChild(li);
+  });
+}
+
+
+
+
+
+
+
+
 
 
 
